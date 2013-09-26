407ca40b8fcf1aee590f214e34b41e3c53fd087b
dc <dcvan24@gmail.com>
2013-09-26 17:44:09 -0400

    1.Adding comments 2.Create directory before downloading eirods package.

--- eirods/eirods_prepare_centos-5.x.sh
+++ eirods/eirods_prepare_centos-5.x.sh
@@ -3,6 +3,10 @@
 # Author: DC
 # Initialize and install eirods iCAT server
 
+# Check if a package is installed, add it to a installing 
+# list if it is not installed
+#
+# $1 - package full name
 is_installed(){ 
 	PKG_NAME=$(echo $1|cut -d'.' -f1)
 	PKG_SEARCH_RESULT=$(yum search $PKG_NAME|grep "^$1")
@@ -15,28 +19,31 @@ is_installed(){
 
 }
 
+# Check dist
 if [ ! "$(grep "CentOS" /etc/redhat-release|egrep "5\.[0-9]{1}")" ];then
 	echo "This script is for CentOS 5.x."
 	echo "Exit."
 	exit 1
 fi
 
+# ID of the process occupying port 5432, which will be used by PostgreSQL
+LISTEN_PID=$(netstat -ntlp|grep 5432|awk '{print $7}'|cut -d'/' -f1 2> /dev/null)
+
+# IP of the machine
+IPADDR=$(ifconfig eth0|grep 'inet addr'|cut -d':' -f2|cut -d' ' -f1|sed 's/[ \t]//g')
+HOSTNAME="icat-$(echo $IPADDR|cut -d'.' -f1)$(echo $IPADDR|cut -d'.' -f4)"
+ICAT_DOWNLOAD="ftp://ftp.renci.org/pub/eirods/releases/3.0/eirods-3.0-64bit-icat-postgres-redhat.rpm"
+ICAT_PATH="/root/build/eirods-3.0-64bit-icat-postgres-redhat.rpm"
+
 echo "Remove user eirods..."
 userdel eirods 2> /dev/null
 echo "Remove legacy data..."
 rm -rf /var/lib/pgsql/data
 
-LISTEN_PID=$(netstat -ntlp|grep 5432|awk '{print $7}'|cut -d'/' -f1 2> /dev/null)
 if [ "$LISTEN_PID" ];then
 	echo "Stop service listening port 5432..."
 	kill -9 $LISTEN_PID
 fi
-
-
-IPADDR=$(ifconfig eth0|grep 'inet addr'|cut -d':' -f2|cut -d' ' -f1|sed 's/[ \t]//g')
-HOSTNAME="icat-$(echo $IPADDR|cut -d'.' -f1)$(echo $IPADDR|cut -d'.' -f4)"
-ICAT_DOWNLOAD="ftp://ftp.renci.org/pub/eirods/releases/3.0/eirods-3.0-64bit-icat-postgres-redhat.rpm"
-ICAT_PATH="/root/build/eirods-3.0-64bit-icat-postgres-redhat.rpm"
 
 echo "Set hostname..."
 hostname $HOSTNAME
@@ -72,6 +79,7 @@ else
 fi
 
 if ! [ -a $ICAT_PATH ];then
+	mkdir -p /root/build
 	wget $ICAT_DOWNLOAD -P /root/build
 fi
 rpm -i /root/build/eirods-3.0-64bit-icat-postgres-redhat.rpm
